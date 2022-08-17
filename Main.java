d600ea5018c8504b79455dcf466c4c0c68cba84c
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-08-17 21:42:07 +0500

    added isEmpty method. closes #5.

--- Main.java
+++ Main.java
@@ -3,5 +3,9 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         Queue queue = new Queue();
+
+        boolean isEmpty = queue.isEmpty();
+
+        System.out.println(isEmpty);
     }
 }
--- Queue.java
+++ Queue.java
@@ -4,4 +4,13 @@ public class Queue {
     public Queue() {
         singlyLinkedList = new SinglyLinkedList();
     }
+
+    /**
+     * This method will tell the queue is empty or not
+     *
+     * @return boolean
+     */
+    public boolean isEmpty() {
+        return singlyLinkedList.getSize() == 0;
+    }
 }
